1bdf02c62f67822949e190423b5c816fdba3ad75
Arsenii Fomin <fomin.artmind@gmail.com>
2023-10-24 12:13:26 +0700

    - Added sending of chat messages on enter pressing; - Added trimming and not empty check for messages to send;

--- src/components/chat-send-box.vue
+++ src/components/chat-send-box.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-2 border-t-2 border-white">
     <div class="flex items-center justify-between">
-      <input placeholder="message other players" class="w-full text-white bg-ancient-400 placeholder-ancient-100 py-1 px-2 focus:outline-none" v-model="text" />
+      <input placeholder="message other players" class="w-full text-white bg-ancient-400 placeholder-ancient-100 py-1 px-2 focus:outline-none" v-model="text" @keyup.enter="send" />
       <button class="bg-ancient-600 hover:bg-ancient-500 rounded-none text-white ml-2 py-1 px-4 focus:outline-none focus:shadow-outline" type="button" @click="send">
         Send
       </button>
@@ -20,8 +20,11 @@ import { sendMessage } from "../composables/state";
 let text = ref("");
 
 const send = () => {
-  sendMessage(text.value);
+  const message = text.value.trim();
+  if(message) {
+    sendMessage(message);
 
-  text.value = "";
+    text.value = "";
+  }
 }
 </script>
